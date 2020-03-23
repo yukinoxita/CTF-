6879994e1c58c7d6d3a7ab55970ed592e3e08532
yukinoxita <m1091057545@163.com>
2020-03-23 23:21:38 +0800

    init

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+?>
+<html>
+<head>
+<title>
+テストへようこそ
+</title>
+<style>
+h1{font-size:50px;}
+p{font-size:30px;}
+</style>
+</head>
+<body>
+<div>
+<h1>规则</h1>
+<p>在规定的时间内解题获取flag,并将解题步骤以word文档的形式记录下来</p>
+<p>个人赛,不允许有相互讨论的情况</p>
+<p><b>ps:题全是我自己出的，网上没有。别抱有幻想。</b></p>
+<p>さあ、盛り上げいこうか！！！</p>
+</div>
+<div id=tesuto>
+    <ul>
+    <a href="1/index.php">第一关</a>
+    <a href="2/index.php">第二关</a>
+    <a href="3/index.php">第三关</a>
+    <a href="4/index.php">第四关</a>
+    <a href="5/index.php">第五关</a>
+    <a href="6/index.php">第六关</a>
+    </ul>
+</div>
+</body>
+</html>
